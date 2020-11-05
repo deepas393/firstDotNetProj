1a1e603ce7230bebcf676ee590fdb4e6ea5f68bb
Deepa <deepa301189@gmail.com>
2020-11-05 14:02:55 +1100

    updated the file

--- firstProj/Startup.cs
+++ firstProj/Startup.cs
@@ -32,7 +32,7 @@ namespace firstProj
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Edited app name");
                 });
             });
         }
